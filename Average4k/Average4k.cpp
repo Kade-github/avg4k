467da72abb609f753ba04fc538e0b834d4ff4da4
KadeCS <KadePcGames@gmail.com>
2022-06-03 21:03:16 -0700

    minors????

--- Average4k/Average4k.cpp
+++ Average4k/Average4k.cpp
@@ -225,7 +225,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 			<< std::endl;
 		exit(1);
 	}
-	Game::version = "b11";
+	Game::version = "b11.0a";
 
 	curl_global_init(CURL_GLOBAL_ALL);
 	Multiplayer::InitCrypto();

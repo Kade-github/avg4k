ab54a037d938e887d8dd5cb01103ce8b0ad2d397
Fruit <10930415+FruitBoi@users.noreply.github.com>
2022-06-27 18:33:10 -0500

    MY BALLS!!!!!!

--- Average4k/MainerMenu.cpp
+++ Average4k/MainerMenu.cpp
@@ -1631,7 +1631,7 @@ void MainerMenu::createNewLobbies(std::string searchTerm)
 	for (lobby& l : menu->Lobbies)
 	{
 		if (searchTerm.size() != 0)
-			if (!l.LobbyName.contains(searchTerm))
+			if (l.LobbyName.find(searchTerm) == std::string::npos)
 				continue;
 		AvgContainer* cont = new AvgContainer(0, 92 * ind, NULL);
 		cont->callback = lobbySelectedCallback;

49ccaa7b53db1d7110c4cedce633b8356bc4f5dc
KadeCS <KadePcGames@gmail.com>
2022-08-24 13:02:24 -0700

    version debump ig

--- Average4k/Average4k.cpp
+++ Average4k/Average4k.cpp
@@ -226,7 +226,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 			<< std::endl;
 		exit(1);
 	}
-	Game::version = "b12.0";
+	Game::version = "b11.1";
 
 	curl_global_init(CURL_GLOBAL_ALL);
 	Multiplayer::InitCrypto();

0042dcf8a1ef321a02dc4b0fe7788249e6d48681
KadeCS <KadePcGames@gmail.com>
2022-10-29 11:40:07 -0700

    jk that one sucks

--- Average4k/Helpers.h
+++ Average4k/Helpers.h
@@ -142,10 +142,16 @@ public:
 
 	static void GetDesktopResolution(int& horizontal, int& vertical)
 	{
-		int width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
-		int height = GetSystemMetrics(SM_CYVIRTUALSCREEN);
-		horizontal = width;
-		vertical = height;
+		RECT desktop;
+		// Get a handle to the desktop window
+		const HWND hDesktop = GetDesktopWindow();
+		// Get the size of screen to the variable desktop
+		GetWindowRect(hDesktop, &desktop);
+		// The top left corner will have coordinates (0,0)
+		// and the bottom right corner will have coordinates
+		// (horizontal, vertical)
+		horizontal = desktop.right;
+		vertical = desktop.bottom;
 	}
 
 	static bool file_exists(const std::string& name) {

094beee1e3b7daac67c277e57b6a62128491362d
Fruit <10930415+FruitBoi@users.noreply.github.com>
2023-01-29 13:25:44 -0600

    lol

--- Average4k/Average4k.cpp
+++ Average4k/Average4k.cpp
@@ -221,7 +221,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		Logging::writeLog("SDL2 video subsystem couldn't be initialized. Error: " + std::string(SDL_GetError()));
 		exit(1);
 	}
-	Game::version = "b13h";
+	Game::version = "b13h2";
 
 	curl_global_init(CURL_GLOBAL_ALL);
 	Multiplayer::InitCrypto();

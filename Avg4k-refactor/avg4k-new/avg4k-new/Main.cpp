e7adcb6c433d045d1dd562ac2f10d3439c52c360
Kade <KadePcGames@gmail.com>
2023-02-06 02:06:06 -0800

    increase the multi-sample

--- Avg4k-refactor/avg4k-new/avg4k-new/Main.cpp
+++ Avg4k-refactor/avg4k-new/avg4k-new/Main.cpp
@@ -151,7 +151,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	glfwWindowHint(GLFW_SAMPLES, 2);
+	glfwWindowHint(GLFW_SAMPLES, 16);
 
 
 	Game* g = new Game("Average4K", "b14");

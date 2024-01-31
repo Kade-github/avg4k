001d3984eae0009d8a979a8e6fc5f7d7ed998ec1
Kade <26305836+Kade-github@users.noreply.github.com>
2024-01-31 13:44:23 -0800

    setup some other stuff

--- Average4k/Assets/Skins/DefaultArrow/Scripts/MainMenu.lua
+++ Average4k/Assets/Skins/DefaultArrow/Scripts/MainMenu.lua
@@ -4,4 +4,9 @@ function create()
     local s = Sprite.new(20,20, "Images/Logo.png")
 
     currentMenu:addObject(s)
+
+end
+
+function draw()
+    --print(tostring(mouse.x) .. " " .. tostring(mouse.y))
 end

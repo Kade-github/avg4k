261758e573f2d29618abbaa46fce32df8c742e3a
Kade <26305836+Kade-github@users.noreply.github.com>
2024-02-01 15:57:35 -0800

    greatest ui design ever

--- Average4k/Assets/Skins/DefaultArrow/Scripts/MainMenu.lua
+++ Average4k/Assets/Skins/DefaultArrow/Scripts/MainMenu.lua
@@ -1,12 +1,9 @@
 function create()
-    print('hello world')
-
     local s = Sprite.new(20,20, "Images/Logo.png")
 
     currentMenu:addObject(s)
 
-end
+    local songs = Text.new(20, s.y + s.height + 20, "Arial.fnt", "> Songs", 24)
 
-function draw()
-    --print(tostring(mouse.x) .. " " .. tostring(mouse.y))
+    currentMenu:addObject(songs)
 end

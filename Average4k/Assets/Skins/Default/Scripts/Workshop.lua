5eeeffc6a66bf001e1c58a97f7056594330b29c2
Kade <26305836+Kade-github@users.noreply.github.com>
2024-02-14 08:10:32 -0800

    text

--- Average4k/Assets/Skins/Default/Scripts/Workshop.lua
+++ Average4k/Assets/Skins/Default/Scripts/Workshop.lua
@@ -1,10 +1,17 @@
 local logo = nil
+local back = nil
 
 function create()
     logo = Sprite.new(20, 20, "Images/Logo.png")
 
     logo.width = math.floor(700 * getWidthScale())
     logo.height = math.floor(314 * getHeightScale())
+
+    currentMenu:addObject(logo)
+
+    back = Text.new(20, logo.y + logo.height + math.floor(20 * getHeightScale()), "Arial.fnt", "Press escape to go back", math.floor(42 * getHeightScale()))
+
+    currentMenu:addObject(back)
 end
 
 function keyPress(data)
